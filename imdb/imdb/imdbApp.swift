9daff1db0a26c5b7e35d39c2576b18fca5b9ab13
Alberto Fernández-Baíllo <alberto.fernandezb@i3television.es>
2022-09-19 18:54:01 +0200

    initial commit

---
+++ imdb/imdb/imdbApp.swift
@@ -0,0 +1,17 @@
+//
+//  imdbApp.swift
+//  imdb
+//
+//  Created by Alberto Fernandez-Baillo Rodriguez on 19/9/22.
+//
+
+import SwiftUI
+
+@main
+struct imdbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
